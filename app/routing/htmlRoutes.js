03a291fbd5d85f15f0449e9b85577d5935ff05d2
Jester <ABooth9422@gmail.com>
2019-07-15 12:09:27 -0400

    fixed pathing error

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,15 +8,15 @@ module.exports= function(app){
 
    
 app.get('/survey',function(req,resp){
-    resp.sendFile(path.join(__dirname,'./public/survey.html'))
+    resp.sendFile(path.join(__dirname,'../public/survey.html'))
 })
 
 app.get('/home',function(req,resp){
-    resp.sendFile(path.join(__dirname,"./public/home.html"))
+    resp.sendFile(path.join(__dirname,"../public/home.html"))
 })
 
-app.get('*',function(req,resp){
-    resp.sendFile(path.join(__dirname,'./public/home.html'))
+app.get('/'||'*',function(req,resp){
+    resp.sendFile(path.join(__dirname,'../public/home.html'))
 })
 
 }
