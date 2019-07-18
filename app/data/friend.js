37720d3ecf2057d35c19feb0608a129b58ff24a8
Jester <ABooth9422@gmail.com>
2019-07-17 20:32:03 -0400

    functionality completed

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,7 +1,39 @@
 
 
 
-var friendArray = [];
+var friendArray = [{
+  "name":"Ahmed",
+  "photo":"https://vignette.wikia.nocookie.net/spongebob/images/d/d7/SpongeBob_stock_art.png/revision/latest?cb=20190604110949",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+},{
+  "name":"assface",
+  "photo":"https://target.scene7.com/is/image/Target/GUEST_f5d0cfc3-9d02-4ee0-a6c6-ed5dc09971d1?wid=488&hei=488&fmt=pjpeg",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+}
+
+];
 
 
   module.exports=friendArray
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,38 @@
-
-
-var path = require('path')
-var friendArray=require('../data/friend')
+var friendArray = require('../data/friend')
 
 
 
-module.exports= function(app){
+module.exports = function (app) {
 
-app.get("/api/friends",function(req,resp){
-    resp.json(friendArray)
-})
+    app.get("/api/friends", function (req, resp) {
+        resp.json(friendArray)
+    })
 
-app.post("/api/friends",function(req,resp){
-    friendArray.push(req.body)
-    
+    app.post("/api/friends", function (req, resp) {
+        req.body.scores = req.body.scores.map((item) => {
+            return parseInt(item)
+        })
+        var match = checker(req.body, friendArray)
+        friendArray.push(req.body)
+        resp.json(match)
 
-})
+        function checker(req, friends) {
+            var match = req
+            var difference = 1000
+            friends.forEach(element => {
+                var total = 0
+                for (let index = 0; index < element.scores.length; index++) {
+                    total += Math.abs(req[index] - element.scores[index])
+                }
+                if (total < difference) {
+                    match = element
+                    difference = total
+                }
+            });
+            return match
+        }
 
-app.get("/bestfriends",function(req,resp){
-    
-    
-})
+    })
+
 
 }
